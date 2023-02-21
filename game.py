6e87fde8fcc1dbf9df013cb6f91606c80b8e0e18
Jean king <jeandedieu405@gmail.com>
2023-02-21 18:18:49 -0500

    create the game calass

--- game.py
+++ game.py
@@ -1,17 +1,9 @@
-import random 
-from game import game
 
+class Game:
+  def __init__(self, name, action):
+        self.name = name
+        self.action = action
 class action():
-Rock = 0
-Paper = 1
-scissors = 2
-Lizard = 3
-Spock = 4
 
-victories = {action.scissors: [action.lizard, action.paper],
-action.paper: [action.spock, action.rock],
-action.rock: [action.lizard, action.paper],
-action.lizard: [action.spock, action.paper],
-action.spock: [action.scissors, action.rock]}
 
 # is this the right section for this ? what do i put in between for and import and in the () for actions
