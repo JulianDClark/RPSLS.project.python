a63e76be82973ef4f7f4220a21e208d55ad61bfc
Jean king <jeandedieu405@gmail.com>
2023-02-23 09:45:44 -0500

    adding welcome message

--- main.py
+++ main.py
@@ -1,53 +1,78 @@
-from player import Player
-from game import Game
+# from game import Game
 # if __name__ == "__main__":
 #     game = Game()
 #     game.run_game()
-class Game:
-    def __init__(self):
-        self.player1 = Player("Player 1")
-        self.player2 = AI()
-        self.choices = ["rock", "paper", "scissors", "lizard", "spock"]
-        self.rules = {"rock": ["scissors", "lizard"],
-                      "paper": ["rock", "spock"],
-                      "scissors": ["paper", "lizard"],
-                      "lizard": ["paper", "spock"],
-                      "spock": ["rock", "scissors"]}
-    def display_greeting(self,greeting):
-        self.display_greeting = greeting
-        print("Welcome to RPSLS")
-        print('')
-        print("Each match will be best 2 out of 3")
-        print('\nRules\nRock crushes Scissors\nScissors cuts Paper\nPaper covers Rock\nRock crushes Lizard\nLizard poisons Spock\nSpock smashes Scissors\nScissors decapitates Lizard\nLizard eats Paper\nPaper disproves Spock\nSpock vaporizes Rock\n ')
-        print("player one please choose one of the chooses")
-        print('rock','paper','scissors','lizard','spock')
+import random
 
+# define the valid choices and their abbreviations
+choices = {"rock": "r", "paper": "p", "scissors": "s", "lizard": "l", "spock": "sp"}
 
-    def determine_winner(self, move1, move2):
-        if move1 == move2:
-            print("It's a tie!")
-        elif move2 in self.rules[move1]:
-            self.player1.add_point()
-            print(f"{self.player1.get_name()} wins!")
+# define the rules of the game: each choice beats the preceding two in the list
+rules = {"rock": ["scissors", "lizard"],
+         "paper": ["rock", "spock"],
+         "scissors": ["paper", "lizard"],
+         "lizard": ["paper", "spock"],
+         "spock": ["rock", "scissors"]}
+
+# function to print the rules of the game
+def print_rules():
+    print("Welcome to RPSLS (Rock, Paper, Scissors, Lizard, Spock)!")
+    print("Here are the rules:")
+    print("- Rock crushes Scissors and crushes Lizard")
+    print("- Paper covers Rock and disproves Spock")
+    print("- Scissors cuts Paper and decapitates Lizard")
+    print("- Lizard eats Paper and poisons Spock")
+    print("- Spock vaporizes Rock and smashes Scissors")
+    print("The first player to win three rounds is the winner.")
+    print()
+
+# function to get the player's choice
+def get_player_choice():
+    while True:
+        choice = input("Enter your choice (r/p/s/l/sp): ").lower()
+        if choice in choices.values():
+            # convert the abbreviation back to the full choice name
+            for c, a in choices.items():
+                if choice == a:
+                    return c
         else:
-            self.player2.add_point()
-            print(f"{self.player2.get_name()} wins!")
+            print("Invalid choice. Please try again.")
 
-    def play_game(self):
-        for i in range(3):
-            print(f"Round {i+1}:")
-            self.player1.set_move(input("Player 1, enter your move: ").lower())
-            self.player2.set_move()
-            print(f"{self.player2.get_name()} chooses {self.player2.get_move()}.")
-            self.determine_winner(self.player1.get_move(), self.player2.get_move())
+# function to get the computer's choice
+def get_computer_choice():
+    return random.choice(list(choices.keys()))
 
-        if self.player1.get_score() > self.player2.get_score():
-            print(f"{self.player1.get_name()} wins the game!")
-        elif self.player2.get_score() > self.player1.get_score():
-            print(f"{self.player2.get_name()} wins the game!")
-        else:
-            print("The game is tied!")
+# function to determine the winner of the game
+def get_winner(player_choice, computer_choice):
+    if player_choice == computer_choice:
+        return "Tie!"
+    elif computer_choice in rules[player_choice]:
+        return "You win!"
+    else:
+        return "Computer wins!"
 
-game = Game() 
-game.display_greeting()
-game.play_game()
+# main game loop
+print_rules()
+player_score = 0
+computer_score = 0
+while player_score < 3 and computer_score < 3:
+    print("Score: Player {} - {} Computer".format(player_score, computer_score))
+    player_choice = get_player_choice()
+    computer_choice = get_computer_choice()
+    print(f"You chose {player_choice}, and the computer chose {computer_choice}.")
+    winner = get_winner(player_choice, computer_choice)
+    print(winner)
+    if winner == "You win!":
+        player_score += 1
+    elif winner == "Computer wins!":
+        computer_score += 1
+print("Final score: Player {} - {} Computer".format(player_score, computer_score))
+if player_score > computer_score:
+    print("You win the game!")
+else:
+    print("Computer wins the game.")
+
+
+# game = Game() 
+# game.display_greeting()
+# game.play_game()
